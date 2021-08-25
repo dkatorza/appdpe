f482d3149a0d8ff7707ff0310a7c46cca7e0cf74
Dan Katorza <dkatorza@gmail.com>
2021-08-25 14:00:42 +0300

    update file structures

---
+++ js/pages/app-home.jsx
@@ -0,0 +1,31 @@
+const { Link } = ReactRouterDOM
+
+export class AppHome extends React.Component {
+
+    render() {
+        return (
+            <div className="main-home flex justify-center column">
+                <div className="title-home text-focus-in">Welcome To AppSus</div>
+                <section className="logo-imgs justify-center flex">
+                    <Link to={`/mail`}>
+                        <div className="logo-container roll-in-blurred-left">
+                            <img className="logo-img" src="./assets/img/mailLogo.svg" />
+                        </div>
+                    </Link>
+                    <Link to={`/keep`}>
+                        <div className="logo-container roll-in-top">
+                            <img className="logo-img" src="./assets/img/keepLogo.png" />
+                        </div>
+                    </Link>
+
+                    <Link to={`/book`}>
+                        <div className="logo-container roll-in-right">
+                            <img className="logo-img" src="./assets/img/bookLogo.svg" />
+                        </div>
+                    </Link>
+
+                </section>
+            </div>
+        )
+    }
+}
