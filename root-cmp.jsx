cb8154193abbac8b196a00e670da8a3d5de7a3c5
Dan Katorza <dkatorza@gmail.com>
2021-08-29 08:30:10 +0300

    bookdetails

--- root-cmp.jsx
+++ root-cmp.jsx
@@ -6,7 +6,7 @@ import { MailApp } from './js/apps/mail/pages/MailApp.jsx';
 import { MailDetails } from './js/apps/mail/pages/mail-details.jsx'
 import { KeepApp } from './js/apps/keep/pages/KeepApp.jsx';
 import { BookApp } from './js/apps/book/pages/BookApp.jsx'
-import { BookDetails } from '.js/apps/book/pages/BookDetails.jsx';
+import { BookDetails } from './js/apps/book/pages/BookDetails.jsx';
 import { AppHome } from './js/pages/app-home.jsx';
 
 
